aea35feeeb5c118ac265ed0de05d9efe842cb4f6
manpreet-codespace <manpreet.dev.85@gmail.com>
2026-03-30 10:03:23 +0530

    update:socket_url

--- src/CallScreen.jsx
+++ src/CallScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { io } from 'socket.io-client'
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000'
+const SOCKET_URL = process.env.VITE_SOCKET_URL || 'http://localhost:5000'
 const socket = io(SOCKET_URL)
 
 const CallScreen = ({ endCall, roomId, joinLink }) => {
